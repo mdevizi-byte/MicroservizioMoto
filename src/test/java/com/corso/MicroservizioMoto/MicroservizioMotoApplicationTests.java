f19de4160d4e4524df434d0eee1e233af6d79271
Mattia De Vizi <mdevizi@studenti.laziodigital.it>
2026-05-06 10:52:03 +0200

    leviaiqwug-<lkdhf

--- src/test/java/com/corso/MicroservizioMoto/MicroservizioMotoApplicationTests.java
+++ src/test/java/com/corso/MicroservizioMoto/MicroservizioMotoApplicationTests.java
@@ -1,4 +1,5 @@
-package com.corso.MicroservizioMoto;
+ssh -T git@github.comgit remote set-url origin https://github.com/<utente>/<repo>.git
+git push -u origin mainpackage com.corso.MicroservizioMoto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
